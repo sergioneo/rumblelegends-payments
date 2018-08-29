df5669c09a1183b0d6244f0c5f256fe5444a438d
Ezequiel Obreque <eandresobrequev@gmail.com>
2018-08-29 01:19:47 -0300

    config

--- src/app/modules/legends/constants/constants.dev.ts
+++ src/app/modules/legends/constants/constants.dev.ts
@@ -7,7 +7,7 @@ const configDev = Object.assign(
 	{
 		COMMON,
 		TIMEOUT: 20000,
-		ENDPOINT: '',
+		ENDPOINT: 'https://beast-service-beta.herokuapp.com/',
 		SERVICE: {
 			ETHER_CLP_DOLAR: 'https://api.coinmarketcap.com/v2/ticker/1027/?convert=CLP',
 			MARKET_PLACE_BEASTS: 'marketplace/get_beasts',
